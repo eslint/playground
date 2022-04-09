ed2ded507e637e2c7340aa590af85fb27752c924
Nitin Kumar <snitin315@gmail.com>
2022-04-09 21:20:27 +0530

    chore: update build script and eleenty config

--- .eleventy.js
+++ .eleventy.js
@@ -22,30 +22,6 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/assets/');
 
-    eleventyConfig.addPassthroughCopy({
-        './src/content/**/*.png': "/assets/images"
-    });
-
-    eleventyConfig.addPassthroughCopy({
-        './src/content/**/*.jpg': "/assets/images"
-    });
-
-    eleventyConfig.addPassthroughCopy({
-        './src/content/**/*.jpeg': "/assets/images"
-    });
-
-    eleventyConfig.addPassthroughCopy({
-        './src/content/**/*.svg': "/assets/images"
-    });
-
-    eleventyConfig.addPassthroughCopy({
-        './src/content/**/*.mp4': "/assets/videos"
-    });
-
-    eleventyConfig.addPassthroughCopy({
-        './src/content/**/*.pdf': "/assets/documents"
-    });
-
     return {
         passthroughFileCopy: true,
 

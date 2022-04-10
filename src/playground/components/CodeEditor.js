19f02bd562ad6e15ad6bfa7ea7025f00e5240767
Nitin Kumar <snitin315@gmail.com>
2022-04-10 18:29:30 +0530

    fix theme

--- src/playground/components/CodeEditor.js
+++ src/playground/components/CodeEditor.js
@@ -1,12 +1,10 @@
 import React from "react";
 import CodeMirror from '@uiw/react-codemirror';
-import { EditorView } from "@codemirror/view";
 import { history } from "@codemirror/history";
-import { tags as t, HighlightStyle} from "@codemirror/highlight"
 import { bracketMatching } from "@codemirror/matchbrackets";
 import { javascript, esLint } from '@codemirror/lang-javascript';
 import { linter } from "../utils/codemirror-linter-extension";
-import { ESLintPlayground } from "../utils/codemirror-theme";
+import { ESLintPlaygroundTheme, ESLintPlaygroundHighlightStyle } from "../utils/codemirror-theme";
 import { Linter as ESLint } from "../node_modules/eslint/lib/linter/";
 import "../scss/editor.scss";
 
@@ -24,22 +22,8 @@ export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions,
                         bracketMatching(),
                         linter(esLint(new ESLint(), eslintOptions), { delay: 0}),
                         javascript(),
-                        ...ESLintPlayground,
-                        // EditorView.theme(
-                        //     {
-                        //         "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
-                        //             backgroundColor: "var(--color-primary-800)",
-                        //             color: "#fff",
-                        //             outline: "1px solid #515a6b"
-                        //         },
-                        //         ".cm-content": {
-                        //             caretColor: "var(--link-color)"
-                        //         },
-                        //         ".cm-cursor, .cm-dropCursor": {
-                        //             borderLeftColor: "var(--link-color)"
-                        //         },
-                        //     }
-                        // ),
+                        ESLintPlaygroundTheme,
+                        ESLintPlaygroundHighlightStyle,
                     ]
                 }
                 onChange={(value) => {
@@ -48,4 +32,4 @@ export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions,
             />
         </>
     );
-}
+};
--- src/playground/utils/codemirror-theme.js
+++ src/playground/utils/codemirror-theme.js
@@ -2,45 +2,70 @@ import {EditorView} from "@codemirror/view"
 import {HighlightStyle, tags as t} from "@codemirror/highlight"
 
 export const ESLintPlaygroundTheme = EditorView.theme({
-  ".cm-content": {
-    caretColor: "var(--link-color)"
-  },
-  "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
-       backgroundColor: "var(--color-primary-800)",
-       color: "#fff",
-       outline: "1px solid #515a6b"
-   },
-   ".cm-content": {
-       caretColor: "var(--link-color)"
-   },
-   ".cm-cursor, .cm-dropCursor": {
-       borderLeftColor: "var(--link-color)"
-   },
+    ".cm-tooltip": {
+        backgroundColor: "transparent",
+        border: "none",
+    },
+    ".cm-scroller": {
+        backgroundColor: "var(--body-background-color)",
+        minWidth: "100%",
+        border: "1px solid var(--body-text-color)",
+        fontFamily: `var(--mono-font), Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace`,
+        fontSize: "1em",
+        color: "var(--body-text-color)",
+    },
+    ".cm-gutter": {
+        paddingRight: "1px",
+        backgroundColor: "var(--body-background-color)",
+    },
+    ".cm-activeLine, .cm-activeLineGutter": {
+        backgroundColor: "var(--body-background-color)",
+    },
+    ".cm-content": {
+        caretColor: "var(--link-color)",
+    },
+    "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
+        backgroundColor: "var(--color-primary-800)",
+        color: "#fff",
+        outline: "1px solid #515a6b"
+    },
+    ".cm-content": {
+        caretColor: "var(--link-color)"
+    },
+    ".cm-cursor, .cm-dropCursor": {
+        borderLeftColor: "var(--link-color)"
+    },
+    ".cm-gutters": {
+        borderRight: "1px solid var(--body-text-color)"
+    },
+    ".cm-gutterElement": {
+        color: "var(--body-text-color)"
+    }
 }, { dark: true });
 
-/// The highlighting style for code in the One Dark theme.
+// The highlighting style for code in the ESLint playground theme.
 export const ESLintPlaygroundHighlightStyle = HighlightStyle.define([
-            {
-                tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
-                color: "var(--link-color)"
-            },
-            {
-                tag: t.strong,
-                fontWeight: "bold"
-            },
-            {
-                tag: t.emphasis,
-                fontStyle: "italic"
-            },
-            {
-                tag: t.strikethrough,
-                textDecoration: "line-through"
-            },
-            {
-                tag: t.heading,
-                fontWeight: "bold",
-                color: "var(--headings-color)"
-            }
-    ]);
+    {
+        tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
+        color: "var(--link-color)"
+    },
+    {
+        tag: t.strong,
+        fontWeight: "bold"
+    },
+    {
+        tag: t.emphasis,
+        fontStyle: "italic"
+    },
+    {
+        tag: t.strikethrough,
+        textDecoration: "line-through"
+    },
+    {
+        tag: t.heading,
+        fontWeight: "bold",
+        color: "var(--headings-color)"
+    }
+]);
 
-export const ESLintPlayground = [ESLintPlaygroundTheme, ESLintPlaygroundHighlightStyle]
+export const ESLintPlayground = [ESLintPlaygroundTheme, ESLintPlaygroundHighlightStyle];

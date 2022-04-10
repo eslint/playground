87c76a08c55860b852f8a7979cc9000cf622a21b
Nitin Kumar <snitin315@gmail.com>
2022-04-10 17:46:02 +0530

    debug

--- src/playground/components/CodeEditor.js
+++ src/playground/components/CodeEditor.js
@@ -6,35 +6,11 @@ import { tags as t, HighlightStyle} from "@codemirror/highlight"
 import { bracketMatching } from "@codemirror/matchbrackets";
 import { javascript, esLint } from '@codemirror/lang-javascript';
 import { linter } from "../utils/codemirror-linter-extension";
-import { oneDarkTheme } from "../utils/codemirror-theme";
+import { ESLintPlayground } from "../utils/codemirror-theme";
 import { Linter as ESLint } from "../node_modules/eslint/lib/linter/";
 import "../scss/editor.scss";
 
 export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions, ...props}) {
-    const myHighlightStyle = HighlightStyle.define([
-            {
-                tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
-                color: "var(--link-color)"
-            },
-            {
-                tag: t.strong,
-                fontWeight: "bold"
-            },
-            {
-                tag: t.emphasis,
-                fontStyle: "italic"
-            },
-            {
-                tag: t.strikethrough,
-                textDecoration: "line-through"
-            },
-            {
-                tag: t.heading,
-                fontWeight: "bold",
-                color: "var(--headings-color)"
-            }
-    ]);
-
     return (
         <>
             <CodeMirror
@@ -46,10 +22,9 @@ export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions,
                     [
                         history(),
                         bracketMatching(),
-                        myHighlightStyle,
                         linter(esLint(new ESLint(), eslintOptions), { delay: 0}),
                         javascript(),
-                        oneDarkTheme,
+                        ...ESLintPlayground,
                         // EditorView.theme(
                         //     {
                         //         "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {

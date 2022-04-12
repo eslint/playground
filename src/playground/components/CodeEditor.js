c617b4b29859f7aa8d0d47b12bc0f820286c8c4f
Nitin Kumar <snitin315@gmail.com>
2022-04-12 06:10:43 +0530

    fix: caret color in dark mode on IOS safari (#18)
    * fix: caret color in dark mode
    
    * fix caret color
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * fix theme

--- src/playground/components/CodeEditor.js
+++ src/playground/components/CodeEditor.js
@@ -1,39 +1,14 @@
 import React from "react";
 import CodeMirror from '@uiw/react-codemirror';
-import { EditorView } from "@codemirror/view";
 import { history } from "@codemirror/history";
-import { tags as t, HighlightStyle} from "@codemirror/highlight"
 import { bracketMatching } from "@codemirror/matchbrackets";
 import { javascript, esLint } from '@codemirror/lang-javascript';
 import { linter } from "../utils/codemirror-linter-extension";
+import { ESLintPlaygroundTheme, ESLintPlaygroundHighlightStyle } from "../utils/codemirror-theme";
 import { Linter as ESLint } from "../node_modules/eslint/lib/linter/";
 import "../scss/editor.scss";
 
 export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions, ...props}) {
-    const myHighlightStyle = HighlightStyle.define([
-            {
-                tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
-                color: "var(--link-color)"
-            },
-            {
-                tag: t.strong,
-                fontWeight: "bold"
-            },
-            {
-                tag: t.emphasis,
-                fontStyle: "italic"
-            },
-            {
-                tag: t.strikethrough,
-                textDecoration: "line-through"
-            },
-            {
-                tag: t.heading,
-                fontWeight: "bold",
-                color: "var(--headings-color)"
-            }
-    ]);
-
     return (
         <>
             <CodeMirror
@@ -45,18 +20,10 @@ export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions,
                     [
                         history(),
                         bracketMatching(),
-                        myHighlightStyle,
                         linter(esLint(new ESLint(), eslintOptions), { delay: 0}),
                         javascript(),
-                        EditorView.theme(
-                            {
-                                "&.cm-focused .cm-matchingBracket, &.cm-focused .cm-nonmatchingBracket": {
-                                    backgroundColor: "var(--color-primary-800)",
-                                    color: "#fff",
-                                    outline: "1px solid #515a6b"
-                                },
-                            }
-                        ),
+                        ESLintPlaygroundTheme,
+                        ESLintPlaygroundHighlightStyle,
                     ]
                 }
                 onChange={(value) => {
@@ -65,4 +32,4 @@ export default function CodeEditor({ codeValue, onUpdate, errors, eslintOptions,
             />
         </>
     );
-}
+};

9a5f0b64b20f3c4afd8b95d38d88b6f529a44463
Nitin Kumar <snitin315@gmail.com>
2022-04-15 05:54:25 +0530

    fix: add title and aria-label attribute

--- src/playground/components/Configuration.js
+++ src/playground/components/Configuration.js
@@ -210,10 +210,14 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                                     <a href={rulesMeta[ruleName].docs.url}>
                                         {`${ruleName} ${rulesMeta[ruleName].deprecated ? "(deprecated)" : ""}`}
                                     </a>
-                                    <button onClick={() => {
-                                        delete options.rules[ruleName];
-                                        onUpdate(Object.assign({}, options));
-                                    }}>
+                                    <button
+                                        aria-label={`Remove ${ruleName}`}
+                                        title={`Remove ${ruleName}`}
+                                        onClick={() => {
+                                            delete options.rules[ruleName];
+                                            onUpdate(Object.assign({}, options));
+                                        }}
+                                    >
                                         <svg width="25" height="25" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M28.5 16L16.5 28M16.5 16L28.5 28" stroke="var(--link-color)" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
                                         </svg>

e6f1e1cada086e1def69d2ebfcaca50e9b138ac6
Nitin Kumar <snitin315@gmail.com>
2022-05-18 18:06:21 +0530

    feat: show runtime error (#41)

---
+++ src/playground/components/CrashAlert.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Alert from "./Alert";
+
+export default function CrashAlert({ error }) {
+    const AlertMessage = (
+        <React.Fragment>
+            <p>
+                ESLint Crashed!
+                <br/>
+                Please submit a bug report at{" "}
+                <a href="https://github.com/eslint/eslint/issues" target="_blank" rel="noreferrer">
+                    https://github.com/eslint/eslint/issues
+                </a>
+            </p>
+            <p>
+                Error: {error.message}
+            </p>
+        </React.Fragment>
+    );
+
+    return (
+        <React.Fragment>
+            <Alert type="error" text={AlertMessage} />
+        </React.Fragment>
+    );
+}

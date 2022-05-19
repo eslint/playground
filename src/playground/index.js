0cf1325f1fa380ee95a84e8c2b5422a8fc32a0cd
amaresh.sm <amareshsm13@gmail.com>
2022-05-19 20:30:04 +0530

    perf: use react v18
    Description:
    1. use React v18
    2. use React strict mode

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("playground-app"));
+const container = document.getElementById("playground-app");
+const root = createRoot(container);
+root.render(<React.StrictMode><App /></React.StrictMode>);

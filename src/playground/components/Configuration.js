52c38caca9a943e3b87d7a584c1c2fc5a54ac612
Nitin Kumar <snitin315@gmail.com>
2022-05-25 16:50:50 +0530

    fix: show error on invalid JSON in rule config (#51)
    * fix: show error on invalid JSON in rule config
    
    * fix: update state when removing rule

--- src/playground/components/Configuration.js
+++ src/playground/components/Configuration.js
@@ -89,6 +89,7 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
     }));
     const [selectedRule, setSelectedRule] = useState();
     const ruleInputRef = useRef(null);
+    const [rulesWithInvalidConfigs, setRulesWithInvalidConfigs] = useState(new Set([]));
 
     return (
         <div className="playground__config-options__sections">
@@ -210,6 +211,7 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                                         title={`Remove ${ruleName}`}
                                         onClick={() => {
                                             delete options.rules[ruleName];
+                                            setRulesWithInvalidConfigs(new Set([...rulesWithInvalidConfigs].filter(rule => rule !== ruleName)));
                                             onUpdate(Object.assign({}, options));
                                         }}
                                     >
@@ -220,19 +222,23 @@ export default function Configuration({ rulesMeta, eslintVersion, onUpdate, opti
                                 </h4>
                                 <input
                                     id={ruleName}
+                                    className={rulesWithInvalidConfigs.has(ruleName) ? "config__added-rules__rule-input-error" : ""}
                                     style={{ width: "100%" }}
                                     defaultValue={JSON.stringify(options.rules[ruleName])}
                                     placeholder={"[\"error\"]"}
                                     onChange={event => {
                                         try {
                                             options.rules[ruleName] = JSON.parse(event.target.value);
+                                            setRulesWithInvalidConfigs(new Set([...rulesWithInvalidConfigs].filter(rule => rule !== ruleName)));
                                             onUpdate(Object.assign({}, options));
                                         } catch {
-
-                                            // ignore
+                                            setRulesWithInvalidConfigs(new Set([...rulesWithInvalidConfigs, ruleName]));
                                         }
                                     }}
                                 />
+                                {rulesWithInvalidConfigs.has(ruleName) && (
+                                    <p className="config__added-rules__rule-error">Invalid rule configuration. Please use a valid JSON format.</p>
+                                )}
                             </RuleItem>
                         ))}
                     </ul>

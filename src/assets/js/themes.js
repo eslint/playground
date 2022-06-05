b8e25754bea103178d1df26246f04eeaf94cc5a1
amaresh.sm <amareshsm13@gmail.com>
2022-06-05 18:10:11 +0530

    fix: avoid theme flash

--- src/assets/js/themes.js
+++ src/assets/js/themes.js
@@ -1,58 +1,42 @@
 /* theme toggle buttons */
 (function() {
-    var enableToggle = function(btn) {
+    var enableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "true");
     }
-
-    var disableToggle = function(btn) {
+    var disableToggle = function (btn) {
         btn.setAttribute("aria-pressed", "false");
+    }
+    var setTheme = function (theme) {
+        document.documentElement.setAttribute('data-theme', theme);
+        window.localStorage.setItem("theme", theme);
     }
 
 
     let theme = window.localStorage.getItem("theme");
-    document.documentElement.setAttribute('data-theme', theme);
-    if (!theme) document.documentElement.setAttribute('data-theme', 'light');
-
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
         var switcher = document.getElementById('js-theme-switcher');
-
-        if(!switcher)
-        return;
-
         switcher.removeAttribute('hidden');
         var light_theme_toggle = document.getElementById('light-theme-toggle'),
             dark_theme_toggle = document.getElementById('dark-theme-toggle');
-
-        // get any previously-chosen themes
-        var theme = window.localStorage.getItem("theme");
-        if (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.documentElement.setAttribute('data-theme', 'dark');
+        if (theme) {
+            if (theme === "light") {
+                enableToggle(light_theme_toggle);
+                disableToggle(dark_theme_toggle);
+            } else if (theme === "dark") {
+                enableToggle(dark_theme_toggle);
+                disableToggle(light_theme_toggle);
+            }
         }
-        else if (theme) document.documentElement.setAttribute('data-theme', theme);
-
-        if (theme == "light") {
-            enableToggle(light_theme_toggle);
-            disableToggle(dark_theme_toggle);
-        } else if (theme == "dark") {
-            enableToggle(dark_theme_toggle);
-            disableToggle(light_theme_toggle);
-        }
-
-        light_theme_toggle.addEventListener("click", function() {
+        light_theme_toggle.addEventListener("click", function () {
             enableToggle(light_theme_toggle);
             theme = this.getAttribute('data-theme');
-            document.documentElement.setAttribute('data-theme', theme);
-            window.localStorage.setItem("theme", theme);
-
+            setTheme(theme);
             disableToggle(dark_theme_toggle);
         }, false);
-
-        dark_theme_toggle.addEventListener("click", function() {
+        dark_theme_toggle.addEventListener("click", function () {
             enableToggle(dark_theme_toggle);
             theme = this.getAttribute('data-theme');
-            document.documentElement.setAttribute('data-theme', theme);
-            window.localStorage.setItem("theme", theme);
-
+            setTheme(theme);
             disableToggle(light_theme_toggle);
         }, false);
     }, false);

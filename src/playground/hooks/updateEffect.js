681290e57ecb4cc638ad0caf55c74bce5a7a8c64
amaresh.sm <amareshsm13@gmail.com>
2022-07-09 02:47:33 +0530

    fix: fix lint errors

--- src/playground/hooks/updateEffect.js
+++ src/playground/hooks/updateEffect.js
@@ -1,21 +1,24 @@
 import { useEffect, useRef } from "react";
 
 export default function useUpdateEffect(effect, dependencies = []) {
-  const render = useRef('first');
-  const isInitialMount = useRef(true);
+    const render = useRef("first");
+    const isInitialMount = useRef(true);
 
-  useEffect(() => {
-    if (isInitialMount.current) {
-        //To handle react scrict mode.
-        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-            if(render.current === 'second')
+    useEffect(() => {
+        if (isInitialMount.current) {
+
+            // To handle react scrict mode.
+            if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+                if (render.current === "second") {
+                    isInitialMount.current = false;
+                }
+                render.current = "second";
+            } else {
                 isInitialMount.current = false;
-            render.current = 'second';
+            }
         } else {
-        isInitialMount.current = false;
+            return effect();
         }
-    } else {
-      return effect();
-    }
-  }, dependencies);
+        return () => {};
+    }, dependencies);
 }
